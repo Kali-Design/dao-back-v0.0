132e12fb4ff8f72339f0d23a60c9223b615c69f6
Kali-Design <hm_leo@hotmail.com>
2021-08-30 10:23:58 +0200

    Update Done

--- contracts/Management.sol
+++ contracts/Management.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.6;
 
+import "./Dao.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 contract Management {
@@ -14,15 +15,28 @@ contract Management {
         uint256 lastPayout;
     }
 
+    event Received(address sender, uint256 amount, uint256 timestamp);
     event Employed(uint256 id, address account, uint256 salary, uint256 timestamp);
     event Fired(uint256 id, address account, uint256 timestamp);
+    event Resigned(uint256 id, address account, uint256 timestamp);
     event Payed(address account, uint256 amount, uint256 time, uint256 timestamp);
 
+    Dao private _dao;
     mapping(address => uint256) private _employeesId;
     mapping(uint256 => Employee) private _employeesData;
     uint256 private _counter;
 
+    constructor() {
+        _dao = Dao(msg.sender);
+    }
+
+    function feed() public payable {
+        emit Received(msg.sender, msg.value, block.timestamp);
+    }
+
     function employ(address account_, uint256 salary_) public returns (bool) {
+        require(_dao.hasRole(_dao.MANAGER_ROLE(), msg.sender), "Management: only Manager Role can use this function");
+        require(account_ != msg.sender, "Management: cannot employ yourself");
         _counter++;
         _employeesId[account_] = _counter;
         _employeesData[_counter] = Employee({
@@ -34,27 +48,45 @@ contract Management {
         emit Employed(_counter, account_, salary_, block.timestamp);
         return true;
     }
+
     function fire(address account) public returns (bool) {
+        require(_dao.hasRole(_dao.MANAGER_ROLE(), msg.sender), "Management: only Manager Role can use this function");
         _employeesData[idOf(account)] = Employee({account: address(0), salary: 0, employedAt: 0, lastPayout: 0});
         _employeesId[account] = 0;
         emit Fired(idOf(account), account, block.timestamp);
         return true;
     }
+
+    function resign() public returns (bool) {
+        _employeesData[idOf(msg.sender)] = Employee({account: address(0), salary: 0, employedAt: 0, lastPayout: 0});
+        _employeesId[msg.sender] = 0;
+        emit Resigned(idOf(msg.sender), msg.sender, block.timestamp);
+    }
+
     function payout() public returns (bool) {
         require(lastPayoutOf(msg.sender) < block.timestamp);
-        uint256 nbPayout = block.timestamp - lastPayoutOf(msg.sender) / INTERVAL;
+        uint256 timestamp = block.timestamp;
+        uint256 nbPayout = timestamp - lastPayoutOf(msg.sender) / INTERVAL;
         uint256 amount = salaryOf(msg.sender) * nbPayout;
+        _employeesData[idOf(msg.sender)].lastPayout = timestamp;
         payable(msg.sender).sendValue(amount);
-        emit Payed(msg.sender, amount, nbPayout, block.timestamp);
+        emit Payed(msg.sender, amount, nbPayout, timestamp);
         return true;
     }
+
     function idOf(address account) public view returns (uint256) {
-        require(_employeesId[account] != 0);
+        require(_employeesId[account] != 0, "Management: this account is not employeed here");
         return _employeesId[account];
     }
+
     function salaryOf(address account) public view returns (uint256) {
         return _employeesData[idOf(account)].salary;
     }
+
+    function employmentOf(address account) public view returns (uint256) {
+        return _employeesData[idOf(account)].employedAt;
+    }
+
     function lastPayoutOf(address account) public view returns (uint256) {
         return _employeesData[idOf(account)].lastPayout;
     }
