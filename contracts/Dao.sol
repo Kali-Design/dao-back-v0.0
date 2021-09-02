5ef8f65ccb29a9396a336ecc16a6a32d3e3a0ed7
Kali-Design <hm_leo@hotmail.com>
2021-09-03 00:51:44 +0200

    Update done - NatSpec

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -7,10 +7,15 @@ import "./Treasury.sol";
 import "./Management.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/// @dev Contract Dao is used to interact with same roles on Governance, Treasury and Management contracts
+/// This contract inherit from AccessControl contract by OpenZeppelin
+
 contract Dao is AccessControl {
     Governance private _governance;
     Treasury private _treasury;
     Management private _management;
+
+    /// @dev Role list for manage AccessControl of this Dao
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
@@ -18,6 +23,11 @@ contract Dao is AccessControl {
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     bytes32 public constant TREASURIER_ROLE = keccak256("TREASURIER_ROLE");
+
+    /// @dev this constructor will deploy 3 others contract and setup the first role
+    /// @param defaultAdmin first admin of this dao
+    /// @param tokenName name of the Govenance Token
+    /// @param tokenSymbol symbol of the Governance Token
 
     constructor(
         address defaultAdmin,
@@ -30,13 +40,19 @@ contract Dao is AccessControl {
         _management = new Management();
     }
 
+    /// @return address of Governance contract
+
     function governanceAddress() public view returns (address) {
         return address(_governance);
     }
 
+    /// @return address of Treasury contract
+
     function treasuryAddress() public view returns (address) {
         return address(_treasury);
     }
+
+     /// @return address of Management contract
 
     function managementAddress() public view returns (address) {
         return address(_management);
